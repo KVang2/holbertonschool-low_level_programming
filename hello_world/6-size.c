0cd15b6fb751bcf46cac9690fa519d6b0da65b33
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-20 12:20:37 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf(("Size of a int: %zu byte(s)\n",sizeof(integerType));
+	printf("Size of a int: %zu byte(s)\n",sizeof(integerType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	printf("Size of a double: %zu byte(s)\n", sizeof(doubleType));
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
