86589f3f36d0abac171565d803ed7f1eef795e28
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-20 12:10:24 -0700

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf(("Size of int: %1d bytes\n", sizeof(integerType));
-	printf("Size of float: %1d bytes\n", sizeof(floatType));
-	printf("Size of double: %1d bytes\n", sizeof(doubleType));
-	printf("Size of char: %1d byte\n", sizeof(charType));
+	printf(("Size of int: %lu\n", sizeof(integerType));
+	printf("Size of float: %lu\n", sizeof(floatType));
+	printf("Size of double: %lu\n", sizeof(doubleType));
+	printf("Size of char: %lu\n", sizeof(charType));
 
 	return (0);
 }
