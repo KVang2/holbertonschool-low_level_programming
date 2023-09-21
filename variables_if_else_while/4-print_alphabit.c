2c873481361e3303582d721f323bb04a667bc10e
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-21 09:11:04 -0700

    alphabet soup

---
+++ variables_if_else_while/4-print_alphabit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - alpabet soup
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z' ; n++)
+	if
+		(n != 'q', 'e' ; n++)
+	else
+		putchar(n);
+
+putchar('\n');
+return (0);
+}
