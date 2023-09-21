7d4825c4c9fe22c70ce658fcf19d7bc093f114a0
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-21 10:04:26 -0700

    numbers 0-10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Numberz
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n <57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n);
+	return (0);
+}
