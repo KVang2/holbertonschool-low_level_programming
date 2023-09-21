d5f371904727d79e54a8f1ec6a275ddff6abfd91
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-21 13:17:19 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * 
+ * main - Hexadecimal
+ *
+ * Return: 0
+ *
+*/
+int main(void)
+
+	int x;
+
+{
+	for (x = 0; x <= 15; x++)
+	{
+		printf("%x", x);
+	}
+	printf("\n");
+	return(0);
+}
