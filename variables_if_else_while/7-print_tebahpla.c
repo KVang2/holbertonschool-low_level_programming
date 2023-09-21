0b36d32ee6e7ee99862d441cfecdc1f1662e5364
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-21 12:12:14 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - reverse loweercase alphabet
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int x
+
+	for (x = 'z'; x <= 'a'; x++)
+	{
+		putchar(x)
+	}
+putchar('\n');
+return(0);
+}
