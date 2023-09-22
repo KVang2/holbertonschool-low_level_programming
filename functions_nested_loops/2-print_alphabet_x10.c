7a4bd6e0ba0482d4407370d6649e86b136d16f97
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-22 09:43:07 -0700

    x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
- *
  * print_alphabet_x10 - printing alphabet x10
  *
  * Return: 0
 */
 void print_alphabet_x10(void)
 {
-	int x = 0 , y;
+	int x = 0, y;
 
 	for (x < 10)
 	{
-		y = 0;
 		for (y = 'a'; y <= 'z'; y++)
 		{
 			_putchar(y);
