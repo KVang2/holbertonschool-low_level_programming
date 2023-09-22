3048e3920c1193e9af23738264ee3afc395c3cc0
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-22 10:01:09 -0700

    10x

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,12 @@ void print_alphabet_x10(void)
 {
 	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
 		for (y = 'a'; y <= 'z'; y++)
 		{
-			if (x == 10)
 			_putchar(y);
 		}
-	}
 	_putchar('\n');
+	}
 }
