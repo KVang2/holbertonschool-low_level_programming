5e6431cf788b78987969c717fb4566717c4ca7e9
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-22 23:42:10 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: lowercase or uppercase
+ *
+ * Return: 1 or 0
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
