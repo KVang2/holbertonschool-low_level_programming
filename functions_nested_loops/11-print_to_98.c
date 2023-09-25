5033e6f285e2ae482b8af8dd855be267e2aba2fb
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-25 06:36:07 -0700

    98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_to_98 - printing numbers
+ *
+ * Return: int n
+*/
+void print_to_98(int n)
+{
+	int n, m;
+	
+	if (n >= 0; n <= 98; n++)
+	{
+		for (m = n; m <= 98; m++)
+		{
+			printf("%d, ", m);
+		}
+		printf('\n');
+	}
+	else if (n < 0)
+	{
+		for (m = n; m <= 98; m++)
+		{
+			printf("%d, ", m);
+		}
+		printf('\n');
+	}
+	else if (n > 98)
+	{
+		for (m = n ; m >= 98 ; m--)
+		{
+			printf("%d, ", m);
+		}
+		printf('\n');
+	}
+	else if (n == 98)
+		printf("%d\n", n);I
+}
