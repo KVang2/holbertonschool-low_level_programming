4c8e19a71cefc4d9e8505f0855144381e88aa4c8
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-24 22:30:31 -0700

    9x

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,34 +1,39 @@
 #include "main.h"
 /**
- * times_table - 9 times table
+ * times_table - times table
  *
- *
- *
- * Return: void
+ * Return: 0
 */
 void times_table(void)
 {
-	int x, y, z;
+	int x, y, z, a, b;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
 			z = x * y;
-			if (y == 0)
+			if (y > 9)
 			{
-				_putchar(z + '0');
+				a= z % 10;
+				b= (z - a) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(b + '0');
+				_putchar(a + '0');
 			}
 			else
 			{
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-				if (y != 9)
+				if (y != 0)
 				{
-					_putchar(", ");
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
 				}
+				_putchar(z + '0');
 			}
 		}
-		_putchar("\n");
+		_putchar('\n');
 	}
 }
