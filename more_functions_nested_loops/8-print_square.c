f4a6d90bdd71b64595f48974670f286d7eb8a0de
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-27 11:52:26 -0700

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - sunshine in square inch
+ *
+ * @size: squares
+ *
+ * Return: 0
+*/
+void print_square(int size);
+{
+	int h, w;
+
+	if (size > 0)
+	{
+		for (w = 0; w < size; w++)
+		{
+		_putchar(35);
+		_putchar('\n');
+		}
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
