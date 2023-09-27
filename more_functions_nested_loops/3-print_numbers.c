5029cce6ba6a015987c862e42b760c9757070f51
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-27 08:37:46 -0700

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - printing the numbers
+ * print_numbers - number of 0 and 9
  *
  * Return: void
 */
@@ -8,7 +8,7 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i + '0');
 		_putchar('\n');
