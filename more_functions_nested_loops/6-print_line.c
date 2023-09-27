6730b2c95c67d5a801e4b68204ae0ba7defcaf24
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-27 09:52:21 -0700

    6 print

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - short distance between two points
+ * @n: point 1
+ *
+ * Return: 0
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n < 0)
+		{
+			_putchar('\n');
+			break;
+		}
+	_putchar(95);
+	_putchar('\n');
+	}
+}
