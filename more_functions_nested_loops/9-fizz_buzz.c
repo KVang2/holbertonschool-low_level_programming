08094fb7ec38e6a7c7a5c7c24b7ada65bb9fc7e7
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-27 12:18:26 -0700

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - fizzbuzzer
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	for x in {1..100}
+	{
+		if (( x % 3 == 0 && x % 5 == 0));
+		{
+			_putchar("FizzBuzz");
+		}
+		else if (( x % 3 == 0 ));
+		{
+			_putchar("Fizz");
+		}
+		else if (( x % 5 == 0 ));
+		{
+			_putchar("Buzz");
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+return (0);
+}
