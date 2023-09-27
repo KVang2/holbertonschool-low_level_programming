99ed69dcde3c237bcdb8d6aed5a43b29401528f4
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-27 07:47:20 -0700

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Checking for uppercase character
+ * @c: uppercase character
+ * Return: 0
+*/
+int _isupper(int c);
+{
+	for (c >= 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
