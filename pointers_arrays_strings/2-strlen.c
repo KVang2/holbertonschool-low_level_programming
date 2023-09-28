0173e4944bf97fe1c20cc5dbbdf381e96effa5f1
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-28 09:06:05 -0700

    test

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -10,13 +10,7 @@ int _strlen(char *s)
 {
 	int l;
 
-	for (l = 0; s[l]; l++)
-	{
-		if (s[l] != 0)
-		{
-		_putchar(*s);
-		}
-	_putchar('\n');
-	}
-return (0);
+	for (l = 0; s[l] != '\0'; l++)
+	
+return (l);
 }
