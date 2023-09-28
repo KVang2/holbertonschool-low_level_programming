4bac986ffaeee850c462aa7b8782f0ceeba3172e
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-27 21:41:34 -0700

    triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - printing a triangle
+ *
+ * @size: amount of triangles
+ *
+ * Return: 0
+*/
+void print_triangle(int size);
+{
+	int h, w;
+
+	for (h = 1; h < size; h++)
+	{
+		for (w = size; w >= 1; w--)
+		{
+			if (h >= w)
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	_putchar('\n');
+	}
+return (0);
+}
