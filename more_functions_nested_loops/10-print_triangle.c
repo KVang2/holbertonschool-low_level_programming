d05cdc65bc639d086406b86799dfd7500924eb99
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-27 22:37:22 -0700

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,19 +10,15 @@ void print_triangle(int size)
 {
 	int h, w;
 
-	if (size > 0)
+	for (h = 1; h <= size; h++)
 	{
-		for (h = 1; h <= size; h++)
+		for (w = h; w < size; w++)
 		{
-			for (w = h; w < size; w++)
-			{
 			_putchar(' ');
-			}
-			for (w = 1; w <= h; w++)
-			{
+		}
+		for (w = 1; w <= h; w++)
+		{
 			_putchar('#');
-			}
-		_putchar('\n');
 		}
 	_putchar('\n');
 	}
