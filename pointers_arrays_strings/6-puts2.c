3d23b7d1d66b63e8794848feccd6da5340e72368
Kevin Vang <kevinvaj88@hotmail.com>
2023-09-28 20:33:31 -0700

    string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - printing every other character
+ *
+ * @str: string
+ *
+ * Return: 0
+*/
+void puts2(char *str)
+{
+	int i;
+
+		for (i = 0; i< s.length(str); i+= 2)
+		{
+		_putchar(str[i]);
+		}
+	}
+	_putchar('\n')
+}
