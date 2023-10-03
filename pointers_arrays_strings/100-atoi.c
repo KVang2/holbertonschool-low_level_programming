cee139687fe372b7c8f530620582b9d66abfd09a
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-02 17:42:59 -0700

    converting into integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,41 +1,31 @@
 #include "main.h"
 /**
- * _atoi - converting a string to an integer
- * @s: infinite number of chars
- * Return: 0
+* int _atoi - string to an integer
+* @s: string
+* Return: 0
 */
 int _atoi(char *s)
 {
-	int i, y, w, m;
+	int c, n, m, i;
 
-	y = 1;
-
-	while (s[i] != '\0')
+	while (s[c])
 	{
-		if (s[i] == '-')
-		{
-			w++;
-		}
-		if (m > 0 '&&' s[i] < '0' '||' s[i] > '9')
-		{
-			break;
-		}
-		if (s[i] >= '0' && (s[i] <= '9'))
-		{
-			if (m * 10 == 2147483640 && s[i] == '8')
-				m = (m * 10) + 7;
-			else
-				m = (m * 10) + (s[i] - '0');
-		}
-		if (w % 2 == 1)
-		{
-			y = -1;
-		}
-		if (m == 2147483647 && y == -1)
-		{
-			m = (m * y);
-				return (m - 1);
-		}
+	if (s[c] == 45)
+	{
+		m *= -1;
 	}
-	return (m * y);
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+		i = 1;
+		n = (n * 10) + (s[c] - '0');
+		c++;
+	}
+		if (i == 1)
+		{
+		break;
+		}
+		c++;
+	}
+		n *= m;
+	return (n);
 }
