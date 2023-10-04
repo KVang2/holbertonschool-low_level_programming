33428e7fda9944bea86c5e8d90079c4a55a07b85
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-04 08:52:51 -0700

    concatenates two strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,19 @@
  * char *_strncat - concatenates two strings
  * @dest: string 1
  * @src: string 2
+ * @n: bytes
  * Return: dest
 */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, dest_len;
 
+	dest_len = strlen(dest);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+		dest_len++;
+	}
+return (dest);
+}
