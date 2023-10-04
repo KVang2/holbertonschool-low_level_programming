83e51e697568b97ea0a706f6668ff30a013a0597
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-04 09:31:46 -0700

    conc

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -15,8 +15,8 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[dest_len + i] = src[i];
-		dest[dest_len + i] = '\0';
+		dest[n] = src[i];
+		n++;
 	}
 return (dest);
 }
