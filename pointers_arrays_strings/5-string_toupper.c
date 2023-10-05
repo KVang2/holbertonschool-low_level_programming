480a0ca0b4260bb705c5fe61e9875423f2ff506b
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-05 15:11:03 -0700

    alpha

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,18 +4,18 @@
  * @a: alphabet letters
  * Return: 0
 */
-char *string_toupper(char *a)
+char *string_toupper(char *str)
 {
 	int i;
 
 	i = 0;
-	while (a[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (a[i] >= 'a' && a[i] <= 'z')
-		{
-			a[i] -= 32;
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+			str[i] -= 32;
 			i++;
-		}
 	}
-	return (a);
+	}
+	return (str);
 }
