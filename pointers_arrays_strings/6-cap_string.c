46fec69559931712349a7814db81d362fbcd6e44
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-05 12:22:13 -0700

    6 caps

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizing words on string
+ * @src: strings
+ * Return: 0
+*/
+char *cap_string(char *src)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(src[i] >= 'a' && src[i] <= 'z'))
+		{
+			i++;
+		}
+		if (src[i - 1] == '' ||
+		src[i - 1] == '\t' ||
+		src[i - 1] == '\n' ||
+		src[i - 1] == ',' ||
+		src[i - 1] == ';' ||
+		src[i - 1] == '.' ||
+		src[i - 1] == '!' ||
+		src[i - 1] == '?' ||
+		src[i - 1] == '"' ||
+		src[i - 1] == '(' ||
+		src[i - 1] == ')' ||
+		src[i - 1] == '{' ||
+		src[i - 1] == '}' ||
+		i == 0)
+			src[i] -= 32;
+
+		i++
+	}
+return (src);
+}
