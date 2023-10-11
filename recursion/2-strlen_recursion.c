a3eab2dee10de8ba588d940d04b1c4312249e2a1
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-11 08:42:10 -0700

    length of string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: input
+ * Return: strlen
+*/
+int _strlen_recursion(char *s)
+{
+	int i;
+	int j = 0;
+
+	if (s[i] != '\0')
+	{
+		return (j);
+	}
+	else
+		j++;
+	_put_strlen_recursion(s, i + 1);
+}
