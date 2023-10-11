1abf878ce3e83a3f63d3421c6f73e2bea5dbdc9c
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-11 12:04:27 -0700

    3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
  * factorial - returns given number
- * @n:
+ * @n: value
  * Return: 0
 */
 int factorial(int n)
 {
 	int i;
 
-	if (n < 0)
+	if (n > 0)
 	{
-		return (-1);
+	 return (n * i(n - 1);
 	}
 	else
 	{
-		return n + i(n - 1);
-	}
+	return (1);
 }
