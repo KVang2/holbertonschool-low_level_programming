8b6f2d74077a82845e5d618ccb4c4b22f1324445
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-11 18:34:00 -0700

    factorial given number

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,13 +6,13 @@
 */
 int factorial(int n)
 {
-	int i;
-
-	if (n > 0)
+	if (n < 0)
 	{
-	 return (n * i(n - 1);
+	return (-1);
 	}
-	else
+	if (n == 0)
 	{
 	return (1);
+	}
+return (n * factorial(n - 1));
 }
