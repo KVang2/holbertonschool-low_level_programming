ead72786e9416adc06c66d15be4cb2f2ee699a12
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-16 11:11:23 -0700

    multiply two integer

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
 	int x = atoi(argv[1]);
 	int y = atoi(argv[2]);
 
-	if (argc == 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", x * y);
+	printf("%d\n", (x * y));
 	return (0);
 }
