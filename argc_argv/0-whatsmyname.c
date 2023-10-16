fdf904c8ff6c3850e8070ccac7a1037c24cca1a3
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-15 22:47:45 -0700

    print new name with new line

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - printing name, followed by new line
- * @argc: number
+ * @agrc: number
  * @argv: array
  * Return: 0
 */
-int main(int agrc __attribute__((unused)), char * argv[])
+int main(int agrc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
