45c50e3a8e422e3254f13b648524268466cc05d7
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-23 16:55:51 -0700

    array of int

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,19 +9,22 @@
 */
 int *array_range(int min, int max)
 {
-	int i, l;
-	int *a;
+	int *a, i = 0;
 
 	if (min > max)
 		return (NULL);
-	l = max - min + 1;
-	a = malloc(sizeof(int) * 1);
+
+	a = malloc((sizeof(int) * (max - min)) + sizeof(int));
+
 	if (a == NULL)
 		return (NULL);
-	for (i = 0; i < l; i++)
+
+	while (min <= max)
 	{
 		a[i] = min;
+		i++;
 		min++;
 	}
+
 	return (a);
 }
