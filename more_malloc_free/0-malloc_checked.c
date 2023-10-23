8f6d5bd10a992ac031967a48f502b01337aefb5d
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-23 10:13:22 -0700

    allocates memory

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocaes memory using malloc
+ * @b: unsigned int
+ * Return: 0
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
