5ef30f57c4d0c108dce9a1aa40ef08288fe2af08
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-25 08:28:47 -0700

    struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - description
+ * @name: dog name, character type
+ * @age: dog age, float type
+ * @owner: dog owner, character type
+*/
 struct dog
 {
 	char *name;
