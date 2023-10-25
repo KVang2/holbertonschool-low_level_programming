50726a0408427551f7b699bc3de67f51e9b4055a
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-25 08:48:42 -0700

    initialize variable type

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * init_dog - initialize a variable type
+ * @d: variable
+ * @name: dog name type char
+ * @age: dog age type float
+ * @owner: dog owner type char
+ * Return: 0
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d;
+
+	d.name = "name";
+	d.age = "age";
+	d.owner = "owner";
+	return (0);
+}
