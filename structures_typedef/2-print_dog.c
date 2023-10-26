9f1408ceddee6a26fe5fef4cbb8e296471f9fbd7
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-26 07:50:33 -0700

    function that prints

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,6 +10,20 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-	return (NULL)
+	return;
 	}
-	if 
+	if (d->name == "(nil)")
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == nil)
+	{
+		d->owner = "(nil)";
+	}
+	else
+	{
+		printf("name %s\n", d->name);
+		printf("age %f\n", d->age);
+		printf("owner %s\n", d->owner);
+	}
+}
