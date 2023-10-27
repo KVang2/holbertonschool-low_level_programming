cc861c0c4a0a41bc59389ee8bf528bdc6732136d
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-27 07:28:55 -0700

    free dogs

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: NULL
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - function that frees dogs
+ * @d: dogs
+ * Return: void
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
