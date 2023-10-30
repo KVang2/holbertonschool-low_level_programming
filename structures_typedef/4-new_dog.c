324030ba65e3e676bc7b9b777794e6b974f45ed1
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-30 09:28:26 -0700

    newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,14 +14,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog = NULL;
 
 	if (name == NULL || owner == NULL)
-	{
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
-	}
 	if (dog == NULL)
-	{
 		return (NULL);
-	}
 	for (n = 0; name[n]; n++)
 	n++;
 	dog->name = malloc(n * sizeof(char));
