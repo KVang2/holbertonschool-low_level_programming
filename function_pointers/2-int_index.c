686d079260eee2aa63a2ae58ade4744fe9760502
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-30 13:33:15 -0700

    searches for integer

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_intdex - searching for an integer
+ * @array: array to search
+ * @size: size of array
+ * @cmp: pointer to the function
+ * Return: index of first element
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+			return (i);
+	}
+	return (-1);
+}
