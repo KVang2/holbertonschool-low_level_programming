d1cea8f3d112dc9280f12b627c865fc8ec3f3274
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-30 08:31:10 -0700

    new dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,9 +15,9 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 
 #endif
