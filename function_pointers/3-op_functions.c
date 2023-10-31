8ce3a736a5a74c4ed0f35b2c7d91182920295417
Kevin Vang <kevinvaj88@hotmail.com>
2023-10-30 22:18:54 -0700

    calculating

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - adding
+ * @a: int 1
+ * @b: int 2
+ * Return: ..
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracting
+ * @a: int 1
+ * @b: int 2
+ * Return: value
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a: int 1
+ * @b: int 2
+ * Return: value
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
