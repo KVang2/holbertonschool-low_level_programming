e67c622b7a5e9bc541899f24cded4c4f0a883cdb
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-09 17:57:28 -0800

    test 2add

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,10 +14,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		return (NULL);
-	}
 	new->next = *head;
 	*head = new;
 return (new);
