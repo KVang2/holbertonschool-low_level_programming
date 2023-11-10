a38bcc42b6fd4d186a02f2fe9c5f30ff71ad84a3
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-09 17:53:08 -0800

    add node test

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node - adds new node at the beginning of a list
+ * @head: node 1
+ * @str: const char
+ * Return: address of new element
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		return (NULL);
+	}
+	new->next = *head;
+	*head = new;
+return (new);
+}
