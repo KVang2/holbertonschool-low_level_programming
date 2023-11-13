789c31ae6ab5f3480b0606eb14a709d0a02ee8f0
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-13 08:42:29 -0800

    sum

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -6,19 +6,16 @@
 */
 int sum_listint(listint_t *head)
 {
-	listint_t *add;
 	int sum = 0;
-
-	add = head;
 
 	if (head == NULL)
 	{
 		return (0);
 	}
-	while (head == NULL)
+	while (head != NULL)
 	{
-		sum += add->n;
-		add = add->next;
+		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
