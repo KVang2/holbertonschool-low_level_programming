5a40ce708f6067597cbec2840587ddab17f3ab82
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-12 22:31:14 -0800

    free list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - ffrees a listint_t list
+ * @head: start of link
+ * Return: void
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->n);
+	}
+}
