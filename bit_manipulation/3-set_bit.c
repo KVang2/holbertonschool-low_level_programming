bd49e7610b6c3ed8cecf6a14cc0da0ad5873832b
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-13 15:00:16 -0800

    set value 1 at index

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,2 +1,16 @@
 #include "main.h"
 /**
+ * set_bit - setting value of a bit to 1
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: 1 or -1
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+	{
+		return (-1);
+	}
+	*n = ((1UL << index) | *n);
+	return (1);
+}
