917634871031e6d83d9e2e2307f4eebae99429b3
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-13 13:45:58 -0800

    binary representation of a number

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,2 +1,14 @@
 #include "main.h"
 /**
+ * print_binary - printing binary representation of a num
+ * @n: unsigned long int
+ * Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	if (n)
+	{
+		print_binary(n >> 1);
+		putchar((n & 1) ? '1' : '0');
+	}
+}
