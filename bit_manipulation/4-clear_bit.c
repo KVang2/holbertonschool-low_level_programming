45fa9943092e701436e84dd0f028bae7f2f1c7ad
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-13 15:44:43 -0800

    set value

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,2 +1,16 @@
 #include "main.h"
 /**
+ * clear_bit - set value of a bit to 0 at given index
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: 1 or -1
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+	{
+		return (-1);
+	}
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
