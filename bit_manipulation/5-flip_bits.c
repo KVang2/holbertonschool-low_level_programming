1fe9e611bbdf1f367355ed8aa28a1217c89ddab7
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-13 16:10:16 -0800

    flip numbers

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,2 +1,21 @@
 #include "main.h"
 /**
+ * flip_bits - returns number of bits you need to flip
+ * @n: unsigned long int
+ * @m: unsigned long int
+ * Return: numbers of flip bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int flip = 0;
+	unsigned long int two;
+
+	two = n ^ m;
+
+	while (two)
+	{
+		flip += two & 1;
+		two >>= 1;
+	}
+	return (flip);
+}
