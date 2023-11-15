3fbadc68aee5dcd78fe724eb8333919b4097f9ef
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-15 08:20:57 -0800

    print ele

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of list
+ * @h: elements
+ * Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (i = 1; h->next != NULL; i++)
+	{
+		if (h->n == NULL)
+		{
+			printf("[%d] %s\n", h->next);
+		}
+		h = h->next;
+	}
+	printf("[%d] %s\n", h->str);
+	return (i);
+}
