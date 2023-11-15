5fa83d09f0649715fe21cfa6d945ca3c919dd992
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-15 08:49:48 -0800

    print

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
-	for (i = 0; h->next != NULL; i++)
+	for (i = 0; h != NULL; i++)
 	{
 		printf("%d \n", h->n);
+		h = h->next;
 	}
 	return (i);
 }
