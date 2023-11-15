e365f9bdd23796a8923483d0c5f8ed5100c70195
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-15 09:27:46 -0800

    adding node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,18 +8,13 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-	link->head = head;
-	link->n = n;
+	new->n = n;
+	new->next = *head;
 
 	if (new == NULL)
 	{
-		last = new;
+		return (NULL);
 	}
-	else
-	{
-		head->prev = new;
-	}
-	new->next = head;
-	head = new;
+	*head = new;
 return (new);
 }
