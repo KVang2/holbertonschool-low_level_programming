b912d6d86131af794939c5a243b996309d72dc56
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-15 09:06:46 -0800

    list length

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * dlistint_len - linking number of elements
+ * @h: start of elements
+ * Return: number of elements in linked list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL, i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
