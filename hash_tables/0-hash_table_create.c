2b536105ae83df059579b92f834a14b0ceeea5b1
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-19 11:33:12 -0800

    hashtables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,9 +14,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 	{
 		return (NULL);
-		table->size = size;
-		table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t*));
 	}
+		table->size = size;
+		table->array = malloc(sizeof(hash_node_t *) * table->size);
 	if (table->array == NULL)
 	{
 		return (NULL);
