bf685e3fa1b6e2f28e9d04296c537fa43510f216
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-19 11:14:24 -0800

    hash

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,14 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 
-	table->size = size;
-	table->array = malloc(table->size, sizeof(hash_node_t));
-
 	if (table == NULL)
 	{
 		return (NULL);
+		table->size = size;
+		table->array = malloc(table->size, sizeof(hash_node_t));
 	}
-	for (int i = 0; i < table->sizes; i++)
+	for (i = 0; i < table->sizes; i++)
 	{
 	table->items[i] = NULL;
 	}
