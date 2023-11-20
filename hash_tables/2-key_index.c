b3debd1993d7e03a806f7a439c9133f92e547ce3
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-20 13:25:48 -0800

    key index

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - function that gives you index of a key
+ * @key: key
+ * @size: size of array
+ * Return: index at key/value
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = hash_djb2(key);
+	return (hash % size);
+}
