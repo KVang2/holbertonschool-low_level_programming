aaaeec49b4332aa31f0d4e3ecd4a16c8e2f8d325
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-20 21:08:54 -0800

    betty check

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -18,8 +18,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	value_copy = strdup(value);
 	if (value_copy == NULL)
-		return(0);
-	index = key_index((const unsigned char*)key, ht->size);
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
 	while (current)
