51af001a444b254dfc92bf505418e6f9563b123a
Kevin Vang <kevinvaj88@hotmail.com>
2023-11-21 05:36:12 -0800

    insert new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,15 +11,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *temp;
 	unsigned int i = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
-
 	if (idx == 0)
 	{
 		new->next = *h;
