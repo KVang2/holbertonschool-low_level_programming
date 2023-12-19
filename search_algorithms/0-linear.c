411f0fe3cc52061997b1e4f38e25fe267f247914
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-18 23:25:37 -0800

    linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,3 +1,22 @@
 #include "main.h"
 /**
- *
+ * linear_search - searches for a value in an array of int
+ * @array: array of int
+ * @size: size of array
+ * @value: value of int
+ * Return: 1st index
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Checking %d with %d\n", array[i], target);
+		if (array[i] == target)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
