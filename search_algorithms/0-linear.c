8aaba130a6126f2a501ec86923c944a0490becfe
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-18 23:35:01 -0800

    error fixing

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -8,11 +8,11 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Checking %d with %d\n", array[i], value);
+		printf("Checking array[%ld] with [%d]\n", array[i]);
 		if (array[i] == value)
 		{
 			return (i);
