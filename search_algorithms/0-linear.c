f1c58e4748400c8383302d87ee73fe7899bab7b1
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-18 23:51:29 -0800

    error issues

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -19,6 +19,7 @@ int linear_search(int *array, size_t size, int value)
 		printf("Checking array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
+			printf("Found at index: %ld\n", i);
 			return (i);
 		}
 	}
