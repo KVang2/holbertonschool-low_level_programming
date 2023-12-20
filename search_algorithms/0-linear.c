a8fd4c6bf587f2d77676507d103ffaed41e49cf7
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-20 14:49:37 -0800

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,17 +10,17 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
-	{
-		return (-1);
-	}
 	for (i = 0; i < size; i++)
 	{
+		printf("checking [%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
 		}
-	printf("Checking [%ld] = [%d]\n", i, array[i]);
-	}
 	return (-1);
+	}
+	if (array == NULL)
+	{
+		return (-1);
+	}
 }
