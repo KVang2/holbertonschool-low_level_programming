de63a9abf6e3aaf7932958a1ee6da6a80b61360a
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-20 18:21:15 -0800

    binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -15,18 +15,20 @@ int binary_search(int *array, size_t size, int value)
 
 	while (left <= right)
 	{
-	mid = left + (right - left) / 2;
 
 		printf("Searching for array: ");
 		for (i = left; i <= right; i++)
 		{
 			printf("%d ", array[i]);
+			if (i < right)
+				printf(", ");
 		}
 		printf("\n");
 
+		mid = left + (right - left) / 2;
 		if (array[mid] == value)
 		{
-			left = mid + 1;
+			return (mid);
 		}
 		if (array[mid] < value)
 		{
@@ -34,7 +36,7 @@ int binary_search(int *array, size_t size, int value)
 		}
 		else
 		{
-			right = mid -1;
+			right = mid - 1;
 		}
 	}
 	return (-1);
